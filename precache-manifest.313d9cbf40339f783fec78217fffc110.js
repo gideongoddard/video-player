8372bd7f2265fead92b274b4a97852b7080718ba
Gideon Goddard <goddard.gcw@gmail.com>
2020-01-20 10:19:33 +0000

    Updates

---
+++ precache-manifest.313d9cbf40339f783fec78217fffc110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d584c5907796c63ab9e65502c7523833",
+    "url": "/video-player/index.html"
+  },
+  {
+    "revision": "f5233d9d933de6d60e5b",
+    "url": "/video-player/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ba19ca973cbe348fade",
+    "url": "/video-player/static/js/2.022ffdf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/video-player/static/js/2.022ffdf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5233d9d933de6d60e5b",
+    "url": "/video-player/static/js/main.f4b95be7.chunk.js"
+  },
+  {
+    "revision": "e4692118a37b53fe1ec6",
+    "url": "/video-player/static/js/runtime-main.52383fdd.js"
+  }
+]);
